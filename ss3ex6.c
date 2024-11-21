75cb9b4d8d4d67183f9d0cdf1847968878b307c8
mthang24242424 <wunkevan@gmail.com>
2024-11-21 20:25:43 +0700

    ss3 ex6

---
+++ ss3ex6.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main(){
+	float doDai, chieuCao;
+	printf("Nhap do dai va chieu cao cua tam giac ");
+	scanf("%f %f",&doDai,&chieuCao);
+	float dienTich =  0.5*doDai*chieuCao;
+	printf("dien tich tam giac la: %.2f",dienTich);
+	return 0;
+ 
+	 
+} 
